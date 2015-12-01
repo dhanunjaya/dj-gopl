27f38231c528e095e4bdca9608fbab860e43b33e
dhanunjaya <dhanunjaya.matrix@gmail.com>
2015-12-01 13:58:12 +0530

    solution of chapter1 - Exercise 1.8

---
+++ ch1/1.8/main.go
@@ -0,0 +1,36 @@
+/* Exercise 1.8: Modify fetch to add the prefix http:// to each argument
+ * URL if it is missing. You might want to use strings.HasPrefix.
+ */
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	urls := os.Args[1:]
+	if len(urls) == 0 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <url1> <url2> ...\n", os.Args[0])
+		os.Exit(1)
+	}
+	for _, url := range urls {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: Copying %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
